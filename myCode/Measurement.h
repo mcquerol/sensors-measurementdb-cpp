ff09cbd4eae07af6667fc511464a1d030212915a
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-04-30 21:49:16 +0200

    added definitions

--- myCode/Measurement.h
+++ myCode/Measurement.h
@@ -8,11 +8,26 @@
 #ifndef MEASUREMENT_H_
 #define MEASUREMENT_H_
 
+#include <stdint.h>
+#include <string>
+
 class Measurement
 {
+private:
+
+	uint16_t timestamp;
+
 public:
-	Measurement();
-	~Measurement();
+	Measurement(uint16_t timestamp);
+	virtual ~Measurement();
+
+	uint16_t toTimestap(int hour, int minute);
+	std::string toTimeOfDay(uint16_t timestamp);
+
+	long getTimestamp() const;
+	virtual std::string toString() const;
+	void parseLine(const std::string& line, uint16_t timestamp, std::string& type, std::string& remainder);
+
 };
 
 #endif /* MEASUREMENT_H_ */
