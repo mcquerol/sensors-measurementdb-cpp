061eb565195b93e3775aacd6a6b67783e56d7a3e
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-01 21:38:04 +0200

    added overload test to print timestamp and temperature

--- myCode/main.cpp
+++ myCode/main.cpp
@@ -10,6 +10,8 @@ int main ()
 
 	Temperature Measurement1(Measurement::toTimestap(6,0), -3);
 	cout << Measurement1.toString() << endl;
+	//overloaded print;
+	cout << Measurement1 << endl;
 
 	return 0;
 }
