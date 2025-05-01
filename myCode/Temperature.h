4bc678fda181aeaff2f279ffbb03cc770c41795a
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-01 03:51:39 +0200

    implemented definitions

--- myCode/Temperature.h
+++ myCode/Temperature.h
@@ -8,10 +8,24 @@
 #ifndef TEMPERATURE_H_
 #define TEMPERATURE_H_
 
-class Temperature
+#include "Measurement.h"
+
+#include <stdint.h>
+#include <string>
+#include <memory>
+
+class Temperature : public Measurement
 {
+private:
+
+	float temperature;
+
 public:
-	Temperature();
+	Temperature(uint16_t timestamp, float temperature);
+	float getTemperature();
+	std::string toString();
+	static std::shared_ptr<Temperature> fromString(uint16_t timestamp, std::string value);
+	static Temperature* toType(Measurement* ptr);
 };
 
 #endif /* TEMPERATURE_H_ */
