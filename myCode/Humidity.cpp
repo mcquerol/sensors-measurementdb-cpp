f09c5a7a19ff03b7ff7f436d2353fc560ac3d3d5
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-01 23:15:34 +0200

    implemented some methods

--- myCode/Humidity.cpp
+++ myCode/Humidity.cpp
@@ -7,9 +7,30 @@
 
 #include "Humidity.h"
 
-Humidity::Humidity()
-{
-	// TODO Auto-generated constructor stub
+using namespace std;
 
+Humidity::Humidity(uint16_t timestamp, float humidity) : Measurement{timestamp}, humidity{humidity}
+{
 }
 
+float Humidity::getHumidity()
+{
+	return humidity;
+}
+
+std::string Humidity::toString()
+{
+	return "Humidity: " + std::to_string(humidity) + " %";
+}
+
+std::shared_ptr<Humidity> Humidity::fromString(uint16_t timestamp, std::string value)
+{
+	shared_ptr<Humidity> ptr; //placeholder
+	return ptr;
+}
+
+Humidity* Humidity::toType(Measurement *ptr)
+{
+	Humidity* temp; //placeholder
+	return temp;
+}
