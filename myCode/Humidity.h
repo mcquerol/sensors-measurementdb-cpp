bfe6466bdcd32ce1e31c5302ec84cb696b6ec52f
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-01 23:15:26 +0200

    defined methods

--- myCode/Humidity.h
+++ myCode/Humidity.h
@@ -8,10 +8,23 @@
 #ifndef HUMIDITY_H_
 #define HUMIDITY_H_
 
-class Humidity
+#include "Measurement.h"
+#include <stdint.h>
+#include <string>
+#include <memory>
+
+class Humidity : public Measurement
 {
+private:
+
+	float humidity;
+
 public:
-	Humidity();
+	Humidity(uint16_t timestamp, float humidity);
+	float getHumidity();
+	std::string toString();
+	static std::shared_ptr<Humidity> fromString(uint16_t timestamp, std::string value);
+	static Humidity* toType(Measurement* ptr);
 };
 
 #endif /* HUMIDITY_H_ */
