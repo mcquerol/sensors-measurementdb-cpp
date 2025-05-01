841d9c34e50a918e48e16ff2c40fb27f24ce768a
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-01 03:51:56 +0200

    added sample test of temperature sensor

--- myCode/main.cpp
+++ myCode/main.cpp
@@ -1,10 +1,17 @@
 
 #include <iostream>
+#include "Measurement.h"
+#include "Temperature.h"
 
 using namespace std;
 
 int main ()
 {
 	cout << "test" << endl;
+
+	Temperature temp(200, 30);
+
+	cout << temp.toString() << endl;
+
 	return 0;
 }
