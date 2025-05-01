2bc6ff7a716889708f061be630dfe0927d7ca74e
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-01 23:29:48 +0200

    added method declarations

--- myCode/MeasurementDb.h
+++ myCode/MeasurementDb.h
@@ -8,8 +8,28 @@
 #ifndef MEASUREMENTDB_H_
 #define MEASUREMENTDB_H_
 
+#include "Measurement.h"
+
+#include <map>
+#include <string>
+#include <vector>
+#include <memory>
+#include <istream>
+#include <ostream>
+
 class MeasurementDb
 {
+private:
+
+	std::map<std::string, std::vector<std::shared_ptr<Measurement>>> data;
+
+public:
+
+	void addMeasurements(const std::string& location, std::shared_ptr<Measurement> measurement);
+	void print() const;
+	void save(std::ostream& to);
+	void load(std::istream& from);
+	bool maximumTemperature(std::string& location, std::shared_ptr<Measurement>& measurement);
 };
 
 #endif /* MEASUREMENTDB_H_ */
